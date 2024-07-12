8954e6ae6e51ab963f40f9087120d3e169425459
magnocarvalho <magno.santos@u4c.com.br>
2024-07-12 17:13:42 -0300

    chore: Update eslint-plugin-prettier to version 5.0.0

--- src/application/api/university.controller.ts
+++ src/application/api/university.controller.ts
@@ -1,22 +1,17 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
-import { MessagePattern, Payload } from '@nestjs/microservices';
-import { UniversityService } from 'src/core/services/university.service';
+import { Body, Controller, Post } from "@nestjs/common";
+import { MessagePattern, Payload } from "@nestjs/microservices";
+import { UniversityService } from "src/core/services/university.service";
 
-@Controller('/universities')
+@Controller("/universities")
 export class UniversityController {
   constructor(private readonly universityService: UniversityService) {}
 
-  @Get()
-  getHello() {
-    return this.universityService.getAllUniversities();
-  }
-
-  @MessagePattern('process_list')
+  @MessagePattern("process_list")
   startProcessList(@Payload() payload: string[]) {
     this.universityService.createWithList(payload);
   }
 
-  @Post('/put-queue')
+  @Post("/put-queue")
   putQueue(@Body() body: { country: string[] }) {
     this.universityService.putInQueue(body.country);
   }
