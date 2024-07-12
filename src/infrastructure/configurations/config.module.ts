5ef324d1044f03c094dc47abdb59be20c3376c1a
magnocarvalho <magno.santos@u4c.com.br>
2024-07-12 16:49:53 -0300

    feat: complete refactor

--- src/infrastructure/configurations/config.module.ts
+++ src/infrastructure/configurations/config.module.ts
@@ -1,16 +1,11 @@
 import { Module } from "@nestjs/common";
 import { ConfigurationEnv } from "./config-enviroments";
 import { ConfigModule } from "@nestjs/config";
-import { HttpModule } from "@nestjs/axios";
 import { WinstonModule } from "nest-winston";
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    HttpModule.register({
-      timeout: 15000,
-      maxRedirects: 5,
-    }),
     WinstonModule.forRoot({
       level: "debug",
     }),
--- src/infrastructure/modules/university.module.ts
+++ src/infrastructure/modules/university.module.ts
@@ -2,11 +2,19 @@ import { UniversityController } from "@application/api/university.controller";
 import { UniversityService } from "@core/services/university.service";
 import { ConfigurationModule } from "@infrastructure/configurations/config.module";
 import { UniversityEntity } from "@infrastructure/database/entities/university.entity";
+import { UniversityRmqProvider } from "@infrastructure/rabbitmq/university-rmq";
+import { HttpModule } from "@nestjs/axios";
 import { Module } from "@nestjs/common";
+import { ClientsModule } from "@nestjs/microservices";
 import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
-  imports: [ConfigurationModule, TypeOrmModule.forFeature([UniversityEntity])],
+  imports: [
+    ConfigurationModule,
+    TypeOrmModule.forFeature([UniversityEntity]),
+    HttpModule,
+    ClientsModule.registerAsync([UniversityRmqProvider]),
+  ],
   controllers: [UniversityController],
   providers: [UniversityService],
 })
